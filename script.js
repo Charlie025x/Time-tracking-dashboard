5c964e9ed8ecbaa16f44e2cfe87141e7389012f8
charlie <charliealonso2001@gmail.com>
2022-03-06 15:59:16 -0600

    attempt to add state rendering, however only last card rerenders

--- script.js
+++ script.js
@@ -16,12 +16,33 @@ fetch("data.json")
       const currentHours = card.querySelector("[data-current-hours]")
       const previousHours = card.querySelector("[data-previous-hours]")
 
+
       renderCards = () => {
       // manipulating DOM w/ data.json
       cardTitle.parentElement.classList.add(cardData.title.replace(/\s+/g, '-').toLowerCase()) // adds class names for tab styles, via data.json, replaces " " for "-" and lower cases the class names
       cardTitle.textContent = cardData.title
-      currentHours.textContent = `${cardData.timeframes.weekly.current}hrs` // TODO: add state
-      previousHours.textContent = `Last Week - ${cardData.timeframes.weekly.previous}hrs` // TODO: add state
+      // currentHours.textContent = `${cardData.timeframes.weekly.current}hrs` // TODO: add state
+      // previousHours.textContent = `Last Week - ${cardData.timeframes.weekly.previous}hrs` // TODO: add state
+
+      // ************************** attempting to add state *************************************
+      if(state.timeFrame === "daily") {
+          currentHours.textContent = `${cardData.timeframes.daily.current}hrs`
+          previousHours.textContent = `Last Week - ${cardData.timeframes.daily.previous}hrs`
+          console.log("daily fired")
+          console.log(cardData)
+      }
+      if(state.timeFrame === "weekly") {
+          currentHours.textContent = `${cardData.timeframes.weekly.current}hrs`
+          previousHours.textContent = `Last Week - ${cardData.timeframes.weekly.previous}hrs`
+          console.log("weekly fire")
+      }
+      if(state.timeFrame === "monthly") {
+          currentHours.textContent = `${cardData.timeframes.monthly.current}hrs`
+          previousHours.textContent = `Last Week - ${cardData.timeframes.monthly.previous}hrs`
+          console.log("monthly fire")
+      }
+
+      // ******************************* end state attempt **************************************
 
       // fill html's card container with template cards
       cardContainer.append(card)
@@ -37,6 +58,9 @@ fetch("data.json")
     timeFrame: "weekly"
   }
 
+  let hourlocator = `cardData.timeframes.${state.timeFrame}`
+  console.log(hourlocator)
+
 // looping though the time frames
 timeFrames.forEach(timeFrame => console.log(timeFrame))
 
@@ -45,16 +69,19 @@ timeFrames.forEach(timeFrame => timeFrame.addEventListener("click", e = () => {
   timeFrame.classList.toggle("active-time-frame") // adds active class on clicked timeframe
   switch (timeFrame) {
     case (timeFrames[0]):
-      console.log("1 is clicked")
+      console.log("daily is clicked")
       state.timeFrame = "daily"
+      renderCards();
       break
     case (timeFrames[1]):
-      console.log("2 is clicked")
+      console.log("weekly is clicked")
       state.timeFrame = "weekly"
+      renderCards();
       break
     case (timeFrames[2]):
-      console.log("3 is clicked")
+      console.log("monthly is clicked")
       state.timeFrame = "monthly"
+      renderCards();
       break
   }
 }))
