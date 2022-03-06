a164e836d1aeda43a6823bc451988df05f121b84
charlie <charliealonso2001@gmail.com>
2022-03-06 12:29:50 -0600

    change state on click. need to render cards with correct data when state is changed

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ fetch("data.json")
       const cardTitle = card.querySelector("[data-card-title]")
       const currentHours = card.querySelector("[data-current-hours]")
       const previousHours = card.querySelector("[data-previous-hours]")
-      
+
+      renderCards = () => {
       // manipulating DOM w/ data.json
       cardTitle.parentElement.classList.add(cardData.title.replace(/\s+/g, '-').toLowerCase()) // adds class names for tab styles, via data.json, replaces " " for "-" and lower cases the class names
       cardTitle.textContent = cardData.title
@@ -24,13 +25,36 @@ fetch("data.json")
 
       // fill html's card container with template cards
       cardContainer.append(card)
+      }
+
+      renderCards()
     })
   });
-
+  
   console.log(timeFrames)
+
+  state = {
+    timeFrame: "weekly"
+  }
 
 // looping though the time frames
 timeFrames.forEach(timeFrame => console.log(timeFrame))
+
 timeFrames.forEach(timeFrame => timeFrame.addEventListener("click", e = () => {
-  console.log("I was clicked")
+  timeFrames.forEach(timeFrame => timeFrame.classList.remove("active-time-frame")) // removes active class on all timeframes
+  timeFrame.classList.toggle("active-time-frame") // adds active class on clicked timeframe
+  switch (timeFrame) {
+    case (timeFrames[0]):
+      console.log("1 is clicked")
+      state.timeFrame = "daily"
+      break
+    case (timeFrames[1]):
+      console.log("2 is clicked")
+      state.timeFrame = "weekly"
+      break
+    case (timeFrames[2]):
+      console.log("3 is clicked")
+      state.timeFrame = "monthly"
+      break
+  }
 }))
