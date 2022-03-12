385d09af7083975ecee63da011f6b73c6c19524b
Charlie025x <charliealonso2001@gmail.com>
2022-03-11 22:42:44 -0600

    deleted create-react-app files, moved card html to a jsx file, began fetch request

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
+import React, { Component } from 'react'
+import Card from './components/card.comonent';
+
 import userImg from './images/image-jeremy.png'
-import iconEllipsis from './images/icon-ellipsis.svg'
 
-function App() {
-  return (
-    <main data-cards-container>
-    <div class="user-card">
-      <div class="user-header">
-        <img src={userImg} alt=""/>
-        <h1>
-          Report for
-          <a>Jeremy Robson</a>
-        </h1>
-      </div>
-      <div data-time-frame class="time-frame">
-        <a>Daily</a>
-        <a class="active-time-frame">Weekly</a>
-        <a>Monthly</a>
-      </div>
-    </div>
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      timeFrame: "weekly",
+      cards: [],
+    }
+  }
 
-    <div class="card work">
-      <h2>Work</h2>
-      <img src={iconEllipsis} alt=""/>
-      <p class="current-hours">
-        32hrs
-      </p>
-      <p class="previous-hours">
-        Previous - 36hrs
-      </p>
-    </div>
-
-  </main>
-  );
+  componentDidMount() {
+    fetch('./data.json')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  }
+  
+  render() {
+    return (
+      <main data-cards-container>
+        <div className="user-card">
+          <div className="user-header">
+            <img src={userImg} alt=""/>
+            <h1>
+              Report for
+              <a>Jeremy Robson</a>
+            </h1>
+          </div>
+          <div data-time-frame className="time-frame">
+            <a>Daily</a>
+            <a className="active-time-frame">Weekly</a>
+            <a>Monthly</a>
+          </div>
+        </div>
+      <Card/>
+    </main>
+    )
+  }
 }
-
-export default App;
