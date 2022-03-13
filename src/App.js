0ac9901b109e81f7be02d8ae967797c3a7d95dc0
Charlie025x <charliealonso2001@gmail.com>
2022-03-12 22:51:59 -0600

    change state on clicking timeframe btns, all cards render correct data using data passed as props

--- src/App.js
+++ src/App.js
@@ -35,18 +35,62 @@ export default class App extends Component {
             <img src={userImg} alt="" />
             <h1>
               Report for
-              <a>Jeremy Robson</a>
+              <span>Jeremy Robson</span>
             </h1>
           </div>
           <div data-time-frame className="time-frame">
-            <a>Daily</a>
-            <a className="active-time-frame">Weekly</a>
-            <a>Monthly</a>
+            <span
+              onClick={(e) => {
+                this.setState(
+                  () => {
+                    return { timeFrame: "daily" };
+                  },
+                  () => {
+                    console.log(this.state);
+                  }
+                );
+              }}
+            >
+              Daily
+            </span>
+            <span
+              onClick={(e) => {
+                this.setState(
+                  () => {
+                    return { timeFrame: "weekly" };
+                  },
+                  () => {
+                    console.log(this.state);
+                  }
+                );
+              }}
+              className="active-time-frame"
+            >
+              Weekly
+            </span>
+            <span
+              onClick={(e) => {
+                this.setState(
+                  () => {
+                    return { timeFrame: "monthly" };
+                  },
+                  () => {
+                    console.log(this.state);
+                  }
+                );
+              }}
+            >
+              Monthly
+            </span>
           </div>
         </div>
         {/* looping through cardsData and rendering cards */}
         {this.state.cardsData.map((cardData) => (
-          <Card key={cardData.title} cardData={cardData} />
+          <Card
+            key={cardData.title}
+            cardData={cardData}
+            timeFrame={this.state.timeFrame}
+          />
         ))}
         {/* ********************************************* */}
       </main>
--- src/components/card.component.jsx
+++ src/components/card.component.jsx
@@ -3,17 +3,33 @@ import iconEllipsis from "../images/icon-ellipsis.svg";
 
 export default class Card extends Component {
   render() {
-    const { cardData } = this.props;
+    // importing card data from data.json and timeframe state
+    const { cardData, timeFrame } = this.props;
     return (
       <div
         className={"card " + cardData.title.replace(/\s+/g, "-").toLowerCase()}
       >
-        {console.log(cardData)}
         <h2>{cardData.title}</h2>
         <img src={iconEllipsis} alt="" />
-        <p className="current-hours">{cardData.timeframes.weekly.current}</p>
+        <p className="current-hours">
+          {timeFrame === "daily"
+            ? cardData.timeframes.daily.current
+            : timeFrame === "weekly"
+            ? cardData.timeframes.weekly.current
+            : timeFrame === "monthly"
+            ? cardData.timeframes.monthly.current
+            : "unKnown timeframe error"}
+          hrs
+        </p>
         <p className="previous-hours">
-          Last Week - {cardData.timeframes.weekly.previous}hrs
+          {timeFrame === "daily"
+            ? "Yesterday - " + cardData.timeframes.daily.previous
+            : timeFrame === "weekly"
+            ? "Last Week - " + cardData.timeframes.weekly.previous
+            : timeFrame === "monthly"
+            ? "Last Month - " + cardData.timeframes.weekly.previous
+            : ""}{" "}
+          hrs
         </p>
       </div>
     );
